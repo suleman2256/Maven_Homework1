eb2fea322a7e2007c56d90f27ee1859715f19cd4
suleiman2256 <suleiman2256@gmail.com>
2021-06-14 01:13:52 +0500

    1) mvn exec:java                  //для вызова основного профиля программы 2) mvn -Plower-case exec:java     //для вызова программы с профилем lower-case

--- Maven/src/main/java/ru/ibs/homework/Main_lower_case.java
+++ Maven/src/main/java/ru/ibs/homework/Main_lower_case.java
@@ -2,10 +2,11 @@ package ru.ibs.homework;
 
 import org.apache.commons.lang3.StringUtils;
 
-public class Main {
+public class Main_lower_case {
     public static void main(String[] args) {
         Greeter greeter = new Greeter();
         System.out.println("1) " + greeter.helloWorld());
-        System.out.println("2) " + StringUtils.upperCase(greeter.helloWorld()));
+        System.out.println("2) " + StringUtils.lowerCase(greeter.helloWorld()));
     }
+
 }
